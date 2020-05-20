e9ef4d74ce0f7e37040640c348c936ce669b265f
Pedro Baiz <p.m.baiz@wiscont.com>
2020-05-20 20:39:57 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo to help identify human activities using IoT and Video data',
+    author='Imperial Students',
+    license='MIT',
+)
